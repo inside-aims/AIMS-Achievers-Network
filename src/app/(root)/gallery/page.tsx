4d39d15bb5d0e22e8ac955e27107677f90cd4c17
Beniako Andrew Nana <b.qhuabenah17@gmail.com>
2025-02-09 18:20:38 +0000

    update
    added the navbar component

--- src/app/(root)/gallery/page.tsx
+++ src/app/(root)/gallery/page.tsx
@@ -1,25 +1,27 @@
-"use client"
+"use client";
 
-import React from 'react'
+import React from "react";
 
-import HeroBanner from '@/components/Gallery/HeroBanner'
-import GalleryGrid from '@/components/Gallery/GalleryGrid'
-import FeaturedCarousel from '@/components/Gallery/FeaturedCarousel'
-import SocialWall from '@/components/Gallery/SocialWall'
-import Footer from '@/components/shared/Footer/Footer'
+import HeroBanner from "@/components/Gallery/HeroBanner";
+import GalleryGrid from "@/components/Gallery/GalleryGrid";
+import FeaturedCarousel from "@/components/Gallery/FeaturedCarousel";
+import SocialWall from "@/components/Gallery/SocialWall";
+import Footer from "@/components/shared/Footer/Footer";
+import { Navbar } from "@/components/shared/Navigation/Navbar";
 
 const page = () => {
   return (
-   <>
-   <main className="min-h-screen bg-award-customBlack">
-      <HeroBanner />
-      <GalleryGrid />
-      <FeaturedCarousel />
-      <SocialWall />
-      <Footer />
-    </main>
-   </>
-  )
-}
+    <>
+      <main className="min-h-screen bg-award-customBlack">
+        <Navbar />
+        <HeroBanner />
+        <GalleryGrid />
+        <FeaturedCarousel />
+        <SocialWall />
+        <Footer />
+      </main>
+    </>
+  );
+};
 
-export default page
+export default page;
